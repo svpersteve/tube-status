000b22b1623bbf864a78b16502108fb0ddf9a8a1
Steve Brewer <steve.brewer@futurelearn.com>
2017-10-08 10:56:13 +0100

    Add some colour

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def colour_for(status)
+    case status
+    when 'Good Service'
+      'green'
+    when 'Part Closure'
+      'orange'
+    when 'Service Closed'
+      'red'
+    else
+      'black'
+    end
+  end
 end
